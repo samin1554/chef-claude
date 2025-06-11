f788393b6fd825e06c0e26c18c56a4a684d0ab61
samin1554 <Samiul27a@gmail.com>
2025-06-10 14:46:16 -1000

    feat: update recipe container to always show with disabled state for <= 3 ingredients

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,35 +1,48 @@
-import { useState } from "react"
+import React from "react"
 
-export default function Body() {
-    const [ingredients, setIngredients] = useState([])
+export default function Main() {
+    const [ingredients, setIngredients] = React.useState([])
 
     const ingredientsListItems = ingredients.map(ingredient => (
         <li key={ingredient}>{ingredient}</li>
     ))
 
     function addIngredient(formData) {
-        const newIngredient = formData.get("ingredients")
-        if (newIngredient.trim()) {  // Only add non-empty ingredients
+        const newIngredient = formData.get("ingredient")
+        if (newIngredient?.trim()) {
             setIngredients(prevIngredients => [...prevIngredients, newIngredient])
         }
     }
 
     return (
         <main>
-            <form className="add-ingredient-form" action={addIngredient}>
+            <form action={addIngredient} className="add-ingredient-form">
                 <input
-                    name="ingredients"
-                    placeholder="Enter your recipe here..." 
                     type="text"
-                    aria-label="Add Ingredients"
+                    placeholder="e.g. oregano"
+                    aria-label="Add ingredient"
+                    name="ingredient"
                 />
-                <button type="submit">Add Ingredient</button>
+                <button type="submit">Add ingredient</button>
             </form>
-            {ingredients.length > 0 && (
-                <ul className="ingredients-list">
-                    {ingredientsListItems}
-                </ul>
-            )}
+            {ingredients.length > 0 && <section>
+                <h2>Ingredients on hand:</h2>
+                <ul className="ingredients-list" aria-live="polite">{ingredientsListItems}</ul>
+                <div className="get-recipe-container">
+                    <div>
+                        <h3>Ready for a recipe?</h3>
+                        <p>Generate a recipe from your list of ingredients.</p>
+                    </div>
+                    <button 
+                        type="button" 
+                        disabled={ingredients.length <= 3}
+                        className={ingredients.length <= 3 ? "disabled" : ""}
+                    >
+                        Get a recipe
+                    </button>
+                </div>
+            </section>}
         </main>
     )
 }
+
